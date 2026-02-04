d2e9b838c23faf535764eef4e320e51e2b4134fb
zuhair-hussain-07 <zuhair.hussain.sg@gmail.com>
2026-02-04 18:56:02 +0800

    Added screenlock to prevent landscape mode

--- app/src/main/java/com/it2161/s243168t/movieviewer/MainActivity.kt
+++ app/src/main/java/com/it2161/s243168t/movieviewer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.it2161.s243168t.movieviewer
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,6 +25,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         enableEdgeToEdge()
         setContent {
             _243168TMovieViewerTheme {
